9533c7067b95173ace5b503b69dfa102e1f77b19
Nastya Leskova <nastyalesk@gmail.com>
2020-06-05 22:35:12 +0500

    Господи! Они убили Кенни!

---
+++ script.ts
@@ -0,0 +1,7 @@
+let drawRectangle = () => {
+    let c = <HTMLCanvasElement>document.getElementById("myCanvas");
+    let ctx = c.getContext("2d");
+    ctx.fillStyle = "red";
+    ctx.fillRect(10, 10, 150, 80);
+}
+document.onload = drawRectangle ;
