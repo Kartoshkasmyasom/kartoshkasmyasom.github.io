bcb04e5847fcf24e7f5977197a59cf6897d0b562
Nastya Leskova <nastyalesk@gmail.com>
2020-06-16 17:26:17 +0500

    Wait

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var drawRectangle = function () {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
     ctx.fillStyle = "red";
-    ctx.fillRect(100, 100, 80, 80);
+    ctx.fillRect(300, 300, 80, 80);
     ctx.stroke();
 };
 drawRectangle();
