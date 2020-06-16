7ff9371dd5b772a579cb53c544684072fe61affa
Nastya Leskova <nastyalesk@gmail.com>
2020-06-16 17:44:20 +0500

    Wait`

--- script.ts
+++ script.ts
@@ -2,7 +2,7 @@ let drawRectangle = () => {
     let c = <HTMLCanvasElement>document.getElementById("myCanvas");
     let ctx = c.getContext("2d");
     ctx.fillStyle = "red";
-    ctx.fillRect(300, 300, 80, 80);
+    ctx.fillRect(100, 100, 80, 80);
     ctx.stroke();
 }
 drawRectangle();
