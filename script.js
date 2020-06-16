65466ac76f4dab41bca1d12e0ea1d75148641853
Nastya Leskova <nastyalesk@gmail.com>
2020-06-16 17:06:15 +0500

    Wait  Please enter the commit message for your changes. Lines starting

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ var drawRectangle = function () {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
     ctx.fillStyle = "red";
-    ctx.fillRect(20, 20, 150, 80);
+    ctx.fillRect(100, 100, 80, 80);
     ctx.stroke();
 };
 drawRectangle();
