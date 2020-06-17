beffb114464e05dbd2ba40d2beb13ed02d4484db
Nastya Leskova <nastyalesk@gmail.com>
2020-06-17 17:55:09 +0500

    Wait

--- script.js
+++ script.js
@@ -2,9 +2,9 @@ var drawRectangle = function () {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
     ctx.fillStyle = "red";
-    for (var i = 0; i < 9; i++) {
-        for (var i_1 = 0; i_1 < 601; i_1 += 75) {
-            ctx.fillRect(i_1, 0, 75, 75);
+    for (var i = 0; i < 9; i += 75) {
+        for (var j = 0; j < 601; j += 75) {
+            ctx.fillRect(j, i, 75, 75);
             ctx.stroke();
         }
     }
--- script.ts
+++ script.ts
@@ -1,12 +1,39 @@
-let drawRectangle = () => {
+let drawRectangle = (string: color, number: i, number: j) => {
     let c = <HTMLCanvasElement>document.getElementById("myCanvas");
     let ctx = c.getContext("2d");
-    ctx.fillStyle = "red";
-    for (let i = 0; i<9; i+=75){
-        for (let j = 0; j < 601; j+=75){
-            ctx.fillRect(j, i, 75, 75);
-            ctx.stroke();
+    ctx.fillStyle = color;
+    ctx.fillRect(75*j, 75*i, 75, 75);
+    ctx.stroke();
+}
+
+
+let firstQuarter = () => {
+    for (let i = 0; i<2; i++){
+        let array = [];
+        for (let j = 0; j<4; j++){
+            array.push("a");
+        }
+        board.push(array);
+    }
+    for (let i = 0; i<2; i++){
+        for (let j = 0; j<4; j++){
+            board[3-i][3-j] = board[i][j];    
         }
     }
-} 
-drawRectangle();
+}
+
+let otherBoard= () => {
+    for (let i = 0; i<4; i++){
+        for (let j = 0; j<4; j++){
+            drawRectangle(board[i][j], i, j);
+            drawRectangle(opp[board[i][j]], i, 7-j);
+            drawRectangle(opp[board[i][j], 7-i, j);
+            drawRectangle(board[i][j], 4+i, 4+j);
+        }
+    }
+}
+
+let board = [["orange", "blue", "purple", "pink"], ["red", "orange", "pink", "green"]];
+let opp = {"orange": "brown", "blue": "green", "purple": "red", "pink": "yellow", "yellow": "pink", "red": "purple", "green": "blue", "brown": "orange"}
+firstQuarter();
+
