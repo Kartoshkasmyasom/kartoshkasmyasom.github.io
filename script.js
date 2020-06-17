47b06efb31c7e1a1b11315702bb920e8825a4d1e
Nastya Leskova <nastyalesk@gmail.com>
2020-06-17 11:03:03 +0500

    Wait

--- script.js
+++ script.js
@@ -2,7 +2,9 @@ var drawRectangle = function () {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
     ctx.fillStyle = "red";
-    ctx.fillRect(300, 300, 80, 80);
-    ctx.stroke();
-};
+    for (let i = 0; i <== 600; i+=75){
+        ctx.fillRect(i, 0, 75, 75);
+        ctx.stroke();
+    }
+}
 drawRectangle();
--- script.ts
+++ script.ts
@@ -2,7 +2,9 @@ let drawRectangle = () => {
     let c = <HTMLCanvasElement>document.getElementById("myCanvas");
     let ctx = c.getContext("2d");
     ctx.fillStyle = "red";
-    ctx.fillRect(100, 100, 80, 80);
-    ctx.stroke();
+    for (let i = 0; i <== 600; i+=75){
+        ctx.fillRect(i, 0, 75, 75);
+        ctx.stroke();
+    }
 }
 drawRectangle();
