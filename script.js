34182210d2367038c222d8e902d76b9f9e00b8e9
Nastya Leskova <nastyalesk@gmail.com>
2020-06-17 11:43:50 +0500

    What

--- script.js
+++ script.js
@@ -2,9 +2,11 @@ var drawRectangle = function () {
     var c = document.getElementById("myCanvas");
     var ctx = c.getContext("2d");
     ctx.fillStyle = "red";
-    for (var i = 0; i <= 600; i+=75){
-        ctx.fillRect(i, 0, 75, 75);
-        ctx.stroke();
+    for (var i = 0; i<9; i++){
+        for (var i = 0; i < 601; i+=75){
+            ctx.fillRect(i, 0, 75, 75);
+            ctx.stroke();
+        }
     }
 };
 drawRectangle();
--- script.ts
+++ script.ts
@@ -2,9 +2,11 @@ let drawRectangle = () => {
     let c = <HTMLCanvasElement>document.getElementById("myCanvas");
     let ctx = c.getContext("2d");
     ctx.fillStyle = "red";
-    for (let i = 0; i <= 600; i+=75){
-        ctx.fillRect(i, 0, 75, 75);
-        ctx.stroke();
+    for (let i = 0; i<9; i++){
+        for (let i = 0; i < 601; i+=75){
+            ctx.fillRect(i, 0, 75, 75);
+            ctx.stroke();
+        }
     }
 } 
 drawRectangle();
