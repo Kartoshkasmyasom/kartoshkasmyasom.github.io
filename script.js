472fb8287d317fb61d35af863b5cacc5d696f699
Nastya Leskova <nastyalesk@gmail.com>
2020-06-20 12:30:41 +0500

    Wait

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ var generateBoard = function () {
     }
 };
 var drawChip = function (x, y, r, color) {
-    var c = document.getElementById("myCanvas");
+    var c = document.getElementById("myChip");
     var ctx = c.getContext("2d");
     ctx.beginPath();
     ctx.arc(x, y, r, 0, 2 * Math.PI);
@@ -44,10 +44,67 @@ var drawChip = function (x, y, r, color) {
     ctx.strokeStyle = color;
     ctx.stroke();
 };
+var Progress = /** @class */ (function () {
+    function Progress() {
+    }
+    Progress.prototype.handleEvent = function (event) {
+        switch (event.type) {
+            case 'mousedown':
+                x = event.clientX;
+                y = event.clientY;
+                X = Math.floor(x / 75);
+                Y = Math.floor(y / 75);
+                if (chipLocation[X][Y] !== "n") {
+                    if (selected !== true) {
+                        selected = true;
+                    }
+                }
+                break;
+            case 'mousemove':
+                document.getElementById("myChip").style.left = x + 'px';
+                document.getElementById("myChip").style.top = y + 'px';
+                x = event.clientX;
+                y = event.clientY;
+                break;
+            case 'mouseup':
+                selected = false;
+                var X1 = Math.floor(event.clientX / 75);
+                var Y1 = Math.floor(event.clientY / 75);
+                if (Y === Y1 && X !== X1 || Math.abs(Y1 - Y) === Math.abs(X1 - X)) {
+                    drawChip(X1 * 75 / 2, Y1 * 75 / 2, 32, "white");
+                    drawChip(X1 * 75 / 2, Y1 * 75 / 2, 16, "orange");
+                }
+                else {
+                    drawChip(X * 75 / 2, Y * 75 / 2, 32, "white");
+                    drawChip(X * 75 / 2, Y * 75 / 2, 16, "orange");
+                }
+                break;
+        }
+    };
+    return Progress;
+}());
 var board = [["orange", "blue", "purple", "pink", "a", "a", "a", "a"], ["red", "orange", "pink", "green", "a", "a", "a", "a"]];
 var opp = { "orange": "brown", "blue": "green", "purple": "red", "pink": "yellow", "yellow": "pink", "red": "purple", "green": "blue", "brown": "orange" };
+var chipLocation = [];
+for (var i = 0; i < 8; i++) {
+    var array = [];
+    for (var j = 0; j < 8; j++) {
+        array.push("n");
+    }
+    chipLocation.push(array);
+}
+var selected = false;
+var X = -1;
+var Y = -1;
+var x = -1;
+var y = -1;
 generateBoard();
 drawBoard();
 drawChip(32.5, 32.5, 32, "white");
 drawChip(32.5, 32.5, 16, "orange");
+chipLocation[0][0] = "orange";
+var progress = new Progress();
+document.addEventListener('mousedown', progress);
+document.addEventListener('mousemove', progress);
+document.addEventListener('mouseup', progress);
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -39,7 +39,7 @@ let generateBoard = () => {
 }
 
 let drawChip = (x, y, r: number, color: string) =>  {
-    let c = document.getElementById("myCanvas");
+    let c = <HTMLCanvasElement>document.getElementById("myChip");
     let ctx = c.getContext("2d");
     ctx.beginPath();
     ctx.arc(x, y, r, 0, 2 * Math.PI);
@@ -49,9 +49,67 @@ let drawChip = (x, y, r: number, color: string) =>  {
     ctx.stroke();
 }
 
+class Progress {
+    handleEvent(event){
+        switch (event.type) {
+            case 'mousedown':
+                x = event.clientX;
+                y = event.clientY;
+                X = Math.floor(x / 75);
+                Y = Math.floor(y / 75);
+                if (chipLocation[X][Y] !== "n") {
+                    if (selected !== true) {
+                        selected = true;
+                    }
+                }
+                break;
+            case 'mousemove':
+                document.getElementById("myChip").style.left = x+'px';
+                document.getElementById("myChip").style.top = y+'px';
+                x = event.clientX;
+                y = event.clientY;
+                break;
+            case 'mouseup':
+                selected = false;
+                let X1 = Math.floor(event.clientX / 75);
+                let Y1 = Math.floor(event.clientY / 75);
+                if (Y === Y1 && X !== X1 || Math.abs(Y1 - Y) === Math.abs(X1 - X)){
+                    drawChip(X1 * 75 / 2, Y1 * 75 / 2, 32, "white");
+                    drawChip(X1 * 75 / 2, Y1 * 75 / 2, 16, "orange");
+                }
+                else{
+                    drawChip(X * 75 / 2, Y * 75 / 2, 32, "white");
+                    drawChip(X * 75 / 2, Y * 75 / 2, 16, "orange");
+                }
+                break;
+        }
+    }
+}
+
 let board = [["orange", "blue", "purple", "pink", "a", "a", "a", "a"], ["red", "orange", "pink", "green", "a", "a", "a", "a"]];
 let opp = {"orange": "brown", "blue": "green", "purple": "red", "pink": "yellow", "yellow": "pink", "red": "purple", "green": "blue", "brown": "orange"}
+let chipLocation = [];
+for (let i = 0; i<8; i++){
+    let array = [];
+    for (let j = 0; j<8; j++){
+        array.push("n");
+    }
+    chipLocation.push(array);
+}
+
+let selected = false;
+let X = -1;
+let Y = -1;
+let x = -1;
+let y = -1;
 generateBoard();
 drawBoard();
 drawChip(32.5, 32.5, 32, "white");
 drawChip(32.5, 32.5, 16, "orange");
+chipLocation[0][0] = "orange";
+let progress = new Progress();
+document.addEventListener('mousedown', progress);
+document.addEventListener('mousemove', progress);
+document.addEventListener('mouseup', progress);
+
+
